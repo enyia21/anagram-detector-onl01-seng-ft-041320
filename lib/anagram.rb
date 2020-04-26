e8a494a040156aab9c9c7634f320b2f41b8e4cd6
Chibuisi Enyia <enyia21@gmail.com>
2020-04-26 21:00:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -18,7 +18,7 @@ class Anagram
 
   def match(anagrams)
     # binding.pry
-    anagrams.collect{|a| a.split("").sort == possible_anagram.split("").sort}
+    anagrams.detect{|a| a.split("").sort == possible_anagram.split("").sort}
   end
 end
 
