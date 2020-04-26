9593a6184a6b211bf4d125f3aa6870358ea5f3ef
Chibuisi Enyia <enyia21@gmail.com>
2020-04-26 20:38:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,6 +3,19 @@ class Anagram
   attr_accessor :possible_anagram
 
   def initalize(anagram_input)
+    @anagram_possibility = anagram_input
+    
+  end
 
+  def possible_anagram
+    @anagram_possibility
+  end
+
+  def possible_anagram=(input)
+    @anagram_possibility = input
+  end
+
+  def match(anagrams)
+    anagrams.detect{|a| a.split.sort == posiible_anagram.split.sort}
   end
 end 
